914c1e3dc36d1efd5a15499cc00a8ed9933bd61f
zion livingstone <zionlivingstone@gmail.com>
2022-09-28 19:41:13 +0100

    new dependencies

---
+++ src/errorpage.jsx
@@ -0,0 +1,18 @@
+import {useRouteError} from 'react-router-dom'
+
+export default function errorPage() {
+    
+    const error=useRouteError()
+    console.error(error)
+    return(
+        <div id="error-page">
+            <h1>Oops! </h1>
+            <p>An unexpected error has occured</p>
+            <p>
+                <i>
+                {error.statusText || error.message}
+                </i>
+            </p>
+        </div>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Root,{loader as rootLoader} from './routes/root'
+import ErrorPage from './errorpage'
+import Contact from './routes/contact'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route
+} from 'react-router-dom'
+import './App.css'
 
+const router=createBrowserRouter([{
+  path: '/',
+  element:<Root />,
+  errorElement:<ErrorPage />,
+  loader:rootLoader,
+  children:[{
+    path: 'contacts/:contactId',
+  element:<Contact />
+  },]
+},
+
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
